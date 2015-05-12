5da5c35be392928e832f63af90e0e56ed0e15881
Espen Klem <espen.klem@gmail.com>
2015-05-12 14:39:14 +0200

    Show filters automatically - sort of...
    You need to hit enter a second time. Good enough for now

--- js/norch-app-jquery.js
+++ js/norch-app-jquery.js
@@ -1,7 +1,7 @@
 //only fires if the the typeahead onSelect function is not invoked
 $('#searchbox').keypress(function(e) {
   if(e.which == 13) {
-    window.location='#/search?q=' + this.value;
+    window.location='#/search?q=' + this.value + '&facets=user,type,tags';
   }
 });
 
